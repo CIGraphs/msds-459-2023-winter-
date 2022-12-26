90fadacd6466ec361605f234e69d52cea3274117
7ten13 <64383269+7ten13@users.noreply.github.com>
2022-12-26 13:30:07 -0800

    example cred file

---
+++ 01_Example_Code/Week_2_Gathering_data_from_API/Python/02_Udemy_basic_Example/cred_user_pwd_keys.py
@@ -0,0 +1,22 @@
+# This file contains the user names passwords, keys etc needed for logging in
+
+# If you are using git make sure to add this to your gitignore or not to upload this file
+
+#Redditt API (Python module praw)
+reddit_client_id="YOUR REDDIT CLIENT ID",#my client id
+reddit_client_secret="YOUR REDDIT CLIENT SECRET",  #your client secret
+reddit_user_agent="YOUR REDDIT AGENT NAME", #user agent name
+reddit_username = "YOUR REDDIT USERNAME",     # your reddit username
+reddit_password = "YOUR REDDIT PASSWORD"
+
+#Udemy Credentials
+Udemy_Client_id='YOUR UDEMY CLIENT ID'
+Udemy_Client_Secret='YOUR UDEMY CLIENT Secret'
+
+#The Movie DB Credentials
+TheMovieDB_API_Key_v3_auth='YOUR TheMovieDB API v3 Key'
+TheMovieDB_API_Read_Access_Token_v4 = 'YOUR TheMovieDB API v4 Token'
+
+#Yelp Credentials
+Yelp_Client_ID='YOUR Yelp Client ID'
+Yelp_API_Key='YOUR Yelp API Key'
